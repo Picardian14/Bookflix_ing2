be294e1dabb1feb79ed0b4fa9822a0e082da0d92
martin-delpino <mdmagne@gmail.com>
2020-06-15 20:46:14 -0300

    novedades

--- flaskps/config/production.py
+++ flaskps/config/production.py
@@ -2,5 +2,5 @@ SECRET_KEY = '\xa6\xbaG\x80\xc9-$s\xd5~\x031N\x8f\xd9/\x88\xd0\xba#B\x9c\xcd_'
 DEBUG = False
 DB_HOST = 'localhost'
 DB_USER = 'grupo21'
-DB_PASS = 'password1'
+DB_PASS = 'lala'
 DB_NAME = 'grupo21'
