44690eab685490efc4daaefded43ef37b3360edd
martin-delpino <mdmagne@gmail.com>
2020-07-15 19:00:04 -0300

    s

--- flaskps/config/production.py
+++ flaskps/config/production.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-SECRET_KEY = '\xa6\xbaG\x80\xc9-$s\xd5~\x031N\x8f\xd9/\x88\xd0\xba#B\x9c\xcd_'
-DEBUG = False
-DB_HOST = 'localhost'
-DB_USER = 'grupo21'
-DB_PASS = 'password1'
-DB_NAME = 'grupo21'
-=======
 SECRET_KEY = '\xa6\xbaG\x80\xc9-$s\xd5~\x031N\x8f\xd9/\x88\xd0\xba#B\x9c\xcd_'
 DEBUG = False
 DB_HOST = 'localhost'
 DB_USER = 'grupo21'
 DB_PASS = 'lala'
 DB_NAME = 'grupo21'
->>>>>>> 97feaabfc4a38a81a336a08a3ffd237c29be98d9
